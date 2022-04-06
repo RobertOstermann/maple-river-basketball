eeccb8db096f41eace565c0b2e8809512fd47c12
Robbie Ostermann <robertostermann5@gmail.com>
2022-04-06 00:14:03 -0500

    Simplify NavBar

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -1,8 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Col, Nav, Navbar, NavItem, Row } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
-import AuthenticationNavBar from "components/navbar/AuthenticationNavBar";
 import RouterHelper, { RouterHelperInterface } from "../Router/RouterHelper";
 
 import styles from "./NavBar.module.scss";
@@ -16,28 +15,22 @@ export default function NavBar() {
       role="navigation"
       variant="dark"
     >
-      <Nav className="w-100">
-        <Row className="w-100">
-          {Object.values(RouterHelper.coach).map(
-            (tab: RouterHelperInterface, index) => (
-              <Col key={`tab-col-coach-${index}`}>
-                <NavItem key={`tab-coach-${index}`}>
-                  <NavLink
-                    to={tab.path}
-                    className={({ isActive }) =>
-                      isActive ? styles.active : styles.link
-                    }
-                  >
-                    <Row className="d-flex flex-column justify-content-center align-items-center">
-                      <FontAwesomeIcon size="lg" icon={tab.icon} />
-                      {tab.label}
-                    </Row>
-                  </NavLink>
-                </NavItem>
-              </Col>
-            )
-          )}
-        </Row>
+      <Nav justify className="w-100">
+        {Object.values(RouterHelper.coach).map(
+          (tab: RouterHelperInterface, index) => (
+            <Nav.Item key={`tab-coach-${index}`}>
+              <NavLink
+                to={tab.path}
+                className={({ isActive }) =>
+                  isActive ? styles.active : styles.link
+                }
+              >
+                <FontAwesomeIcon size="lg" icon={tab.icon} />
+                <div>{tab.label}</div>
+              </NavLink>
+            </Nav.Item>
+          )
+        )}
       </Nav>
     </Navbar>
   );
