336df72f5039e3329f575dc73544ed6ca935ab0f
Robbie Ostermann <robertostermann5@gmail.com>
2022-04-06 17:01:01 -0500

    Protect all routes

--- client/src/components/Router/Router.tsx
+++ client/src/components/Router/Router.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "../auth0/ProtectedRoute";
 import Profile from "../Profile";
 import Request from "../Request";
-import Temp from "../Temp";
 import {
   ADMINISTRATOR_ROUTES,
   COACH_ROUTES,
@@ -13,26 +12,41 @@ import {
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Request />} />
+      <Route path="/" element={<ProtectedRoute component={Request} />} />
       {/* Administrator Routes */}
-      <Route path={ADMINISTRATOR_ROUTES.home.path} element={<Temp />} />
+      <Route
+        path={ADMINISTRATOR_ROUTES.home.path}
+        element={<ProtectedRoute component={Request} />}
+      />
       {/* Coach Routes */}
-      <Route path={COACH_ROUTES.home.path} element={<Temp />} />
-      <Route path={COACH_ROUTES.players.path} element={<Request />} />
-      <Route path={COACH_ROUTES.leaders.path} element={<Profile />} />
+      <Route
+        path={COACH_ROUTES.home.path}
+        element={<ProtectedRoute component={Request} />}
+      />
+      <Route
+        path={COACH_ROUTES.players.path}
+        element={<ProtectedRoute component={Request} />}
+      />
+      <Route
+        path={COACH_ROUTES.leaders.path}
+        element={<ProtectedRoute component={Request} />}
+      />
       <Route
         path={COACH_ROUTES.entries.path}
-        element={<ProtectedRoute component={Temp} />}
+        element={<ProtectedRoute component={Request} />}
       />
       <Route
         path={COACH_ROUTES.profile.path}
-        element={<ProtectedRoute component={Request} />}
+        element={<ProtectedRoute component={Profile} />}
       />
       {/* Player Routes */}
-      <Route path={PLAYER_ROUTES.home.path} element={<Profile />} />
+      <Route
+        path={PLAYER_ROUTES.home.path}
+        element={<ProtectedRoute component={Request} />}
+      />
       <Route
         path={PLAYER_ROUTES.history.path}
-        element={<ProtectedRoute component={Temp} />}
+        element={<ProtectedRoute component={Request} />}
       />
       <Route
         path={PLAYER_ROUTES.newEntry.path}
