296c69a355850d2d5351847a918993cd93242324
Robbie Ostermann <robertostermann5@gmail.com>
2022-04-20 10:12:26 -0500

    Update coach and player home to show total duration

--- client/src/components/pages/coaches/Home/CoachHome.tsx
+++ client/src/components/pages/coaches/Home/CoachHome.tsx
@@ -15,6 +15,7 @@ import styles from "./CoachHome.module.scss";
 export default function CoachHome() {
   const [isLoading, setIsLoading] = useState(false);
   const [totals, setTotals] = useState<any[]>([]);
+  const [totalDuration, setTotalDuration] = useState<number>(0);
 
   const { getAccessTokenSilently } = useAuth0();
 
@@ -28,6 +29,7 @@ export default function CoachHome() {
       const token = await getAccessTokenSilently();
       const entries: EntryModel[] = await EntryRequests.getAllEntries(token);
       const updatedTotals: any[] = [];
+      let totalDuration = 0;
 
       Object.values(ActivityTypes).map(
         (activityType: ActivityTypeInterface) => {
@@ -37,6 +39,7 @@ export default function CoachHome() {
 
       entries.map((entry) => {
         if (entry.activityType !== undefined) {
+          totalDuration += entry.activityDuration ? entry.activityDuration : 0;
           if (updatedTotals[entry.activityType]) {
             updatedTotals[entry.activityType] += entry.activityDuration;
           } else {
@@ -46,6 +49,7 @@ export default function CoachHome() {
       });
 
       setTotals(updatedTotals);
+      setTotalDuration(totalDuration);
     } catch (error) {
       console.log(error);
     }
@@ -73,12 +77,12 @@ export default function CoachHome() {
     return `${hoursString}${minutesString}`;
   };
 
-  const entryCards = () => {
+  const categoryCards = () => {
     return totals.map((duration, activityType) => {
       return (
         <Card
           key={activityType}
-          className={styles.entryCard}
+          className={styles.categoryCard}
           bg="primary-dark"
           border="primary-light"
           text="secondary"
@@ -88,7 +92,7 @@ export default function CoachHome() {
               <Col>Activity</Col>
               <Col>{getActivityType(activityType ?? 0)}</Col>
             </Row>
-            <hr className={styles.entryHR} />
+            <hr className={styles.categoryHR} />
             <Row>
               <Col>Total Duration</Col>
               <Col>{getDurationString(duration ?? 0)}</Col>
@@ -97,6 +101,29 @@ export default function CoachHome() {
         </Card>
       );
     });
+  };
+
+  const totalCard = () => {
+    return (
+      <Card
+        key="combined"
+        className={styles.categoryCard}
+        bg="primary-dark"
+        border="primary-light"
+        text="secondary"
+      >
+        <Card.Body>
+          <Row className={styles.totalRow}>
+            <Col>All Activities</Col>
+          </Row>
+          <hr className={styles.categoryHR} />
+          <Row>
+            <Col>Total Duration</Col>
+            <Col>{getDurationString(totalDuration ?? 0)}</Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    );
   };
 
   if (isLoading) {
@@ -108,7 +135,10 @@ export default function CoachHome() {
       <div className={styles.headerDiv}>
         <h2>Team Totals</h2>
       </div>
-      <div className={styles.entryDiv}>{entryCards()}</div>
+      <div className={styles.categoryDiv}>
+        {totalCard()}
+        {categoryCards()}
+      </div>
     </Container>
   );
 }
