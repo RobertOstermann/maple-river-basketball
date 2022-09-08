aa699a87ac3231b3f07ba2eb16303673257a85d8
Robbie Ostermann <robertostermann5@gmail.com>
2022-09-08 13:33:09 -0500

    Fix problem with REACT environment variables

--- client/src/components/auth0/AuthenticationProviderWithHistory.tsx
+++ client/src/components/auth0/AuthenticationProviderWithHistory.tsx
@@ -2,9 +2,9 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 const AuthenticationProviderWithHistory = ({ children }: any) => {
-  const audience = process.env.REACT_APP_AUTH0_AUDIENCE ?? "";
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? "";
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? "";
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE ?? "https://maple-river-basketball.com/api/v1";
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? "maple-river-basketball.us.auth0.com";
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? "Xzac7ckkszykz9YporyR3DbTiAKoT5EV";
 
   const navigate = useNavigate();
 
