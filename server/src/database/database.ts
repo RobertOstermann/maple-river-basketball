ece84ab4a824606156fdc7fcb413963a4f413ba0
Robert Ostermann <loothunder@gmail.com>
2022-10-01 13:34:22 -0500

    Update gitignore (#4)
    * temp heroku build
    
    * Initial download stats setup
    
    * Add an option for coaches to download player statistics.
    
    * Update .gitignore and database env vars
    
    Co-authored-by: Robbie Ostermann <robertostermann5@gmail.com>

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -4,13 +4,13 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const environment: string = process.env.NODE_ENV ?? "development";
+const environment: string = process.env.NODE_ENV || "development";
 
 let connectionString = "";
 if (environment === "production") {
-  connectionString = process.env.DATABASE_URL ?? "";
+  connectionString = process.env.DATABASE_URL || "";
 } else {
-  connectionString = process.env.DEVELOPMENT_DATABASE_URL ?? "";
+  connectionString = process.env.DEVELOPMENT_DATABASE_URL || "";
 }
 
 const config: pg.ConnectionConfig = {
