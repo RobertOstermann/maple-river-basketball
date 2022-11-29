cd5e9b96f288b4589894698c9bc61da98c5b9768
Robbie Ostermann <robertostermann5@gmail.com>
2022-11-29 09:55:03 -0600

    Revert build in server to tsc

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 
 import * as Auth0 from "auth0";
-import jwt from "express-jwt";
+import { expressjwt } from "express-jwt";
 import jwks from "jwks-rsa";
 
 export default class AuthController {
-  static jwtCheck = jwt({
+  static options: any = {
     secret: jwks.expressJwtSecret({
       cache: true,
       rateLimit: true,
@@ -15,19 +15,21 @@ export default class AuthController {
     audience: process.env.REACT_APP_AUTH0_AUDIENCE,
     issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
     algorithms: ["RS256"],
-  });
+  };
+
+  static jwtCheck = expressjwt(AuthController.options);
 
   static getAccessToken = (): Promise<string> => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const auth0 = new Auth0.AuthenticationClient({
-      domain: process.env.REACT_APP_AUTH0_DOMAIN,
-      clientId: process.env.AUTH0_API_CLIENT_ID,
-      clientSecret: process.env.AUTH0_API_CLIENT_SECRET
+      domain: process.env.REACT_APP_AUTH0_DOMAIN || "",
+      clientId: process.env.AUTH0_API_CLIENT_ID || "",
+      clientSecret: process.env.AUTH0_API_CLIENT_SECRET || ""
     });
 
     return new Promise((resolve, reject) => {
       auth0.clientCredentialsGrant({
-        audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+        audience: process.env.REACT_APP_AUTH0_AUDIENCE || "",
       }, (error: Error, res: Auth0.TokenResponse) => {
         if (error) {
           reject(error);
@@ -40,9 +42,9 @@ export default class AuthController {
 
   static getUserInformation = async (userId: string): Promise<Auth0.User> => {
     const auth0 = new Auth0.ManagementClient({
-      domain: process.env.REACT_APP_AUTH0_DOMAIN,
-      clientId: process.env.AUTH0_API_CLIENT_ID,
-      clientSecret: process.env.AUTH0_API_CLIENT_SECRET
+      domain: process.env.REACT_APP_AUTH0_DOMAIN || "",
+      clientId: process.env.AUTH0_API_CLIENT_ID || "",
+      clientSecret: process.env.AUTH0_API_CLIENT_SECRET || ""
     });
 
     const userObject: Auth0.ObjectWithId = {
